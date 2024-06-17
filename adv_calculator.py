5ce88df0b84c049b1371137d030185e8953f2a6b
navmi-png <navmi605@gmail.com>
2024-06-18 00:01:45 +0530

    file2 version

---
+++ adv_calculator.py
@@ -0,0 +1,53 @@
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    return x / y
+def percentage(x,y):
+    a=y*(x/100)
+    return a
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Percentage")
+
+while True:
+    choice = input("Enter choice(1/2/3/4/5): ")
+
+    if choice in ('1', '2', '3', '4','5'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        elif choice == '5':
+            print(num1, '%', num2, "=", percentage(num1, num2))
+
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+            break
+    else:
+        print("Invalid Input")
